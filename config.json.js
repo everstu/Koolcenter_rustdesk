a028c37d78914525289c3e43aaf5d7a67e2e6a8e
sadog <sadoneli@gmail.com>
2023-06-20 11:01:26 +0800

    just update icon for better looking

--- config.json.js
+++ config.json.js
@@ -3,9 +3,9 @@
     "description": "RustDesk是一款优秀的免费开源的远程控制软件，此插件提供RustDesk自建服务器功能。", 
     "home_url": "Module_rustdesk.asp", 
     "link": "https://github.com/everstu/Koolcenter_rustdesk", 
-    "md5": "c8b02d0df866e9e53d28f61d49012ca5", 
+    "md5": "9598e0bf7f96e918726a18bbcdfd8615", 
     "module": "rustdesk", 
     "tags": "穿透", 
     "title": "RustDesk Server", 
-    "version": "1.1"
+    "version": "1.2"
 }
