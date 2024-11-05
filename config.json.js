24be902792070b968ed23128b459996a0d812efa
WengHeng <whtony2003@163.com>
2024-11-05 17:40:47 +0800

    bump bin ver to v1.1.12

--- config.json.js
+++ config.json.js
@@ -3,9 +3,9 @@
     "description": "RustDesk是一款优秀的免费开源的远程控制软件，此插件提供RustDesk自建服务器功能。", 
     "home_url": "Module_rustdesk.asp", 
     "link": "https://github.com/everstu/Koolcenter_rustdesk", 
-    "md5": "2115104f1dce5d9fe38e55f30157f3ce", 
+    "md5": "99ba3f8b1dff5c5878022514f400d6cc", 
     "module": "rustdesk", 
     "tags": "穿透", 
     "title": "RustDesk Server", 
-    "version": "1.4.5"
+    "version": "1.4.6"
 }
