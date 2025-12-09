0075cc64cda261074692d3cd02efb7d32fc1d8a6
everstu <whtony2003@163.com>
2025-12-09 17:28:27 +0800

    fix css/js error

--- config.json.js
+++ config.json.js
@@ -3,9 +3,9 @@
     "description": "RustDesk是一款优秀的免费开源的远程控制软件，此插件提供RustDesk自建服务器功能。", 
     "home_url": "Module_rustdesk.asp", 
     "link": "https://github.com/everstu/Koolcenter_rustdesk", 
-    "md5": "2c319b6a3df5a4c2d0bd581d7e006764", 
+    "md5": "e115cdbf366aff5782d3edc586cc0141", 
     "module": "rustdesk", 
     "tags": "穿透", 
     "title": "RustDesk Server", 
-    "version": "1.4.7"
+    "version": "1.4.8"
 }
